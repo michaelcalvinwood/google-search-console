89203c4f374041507e701247f70415cad442bc32
Michael Wood <michaelwood33311@icloud.com>
2022-12-26 10:55:54 -0500

    added sending events to google analytics from node

--- getStats.js
+++ getStats.js
@@ -1,11 +1,16 @@
+require('dotenv').config();
+
 const {google} = require('googleapis');
 const {JWT} = require('google-auth-library');
 const searchconsole = google.searchconsole('v1');
 const keys = require('./clientSecret.json');
-require('dotenv').config();
+const axios = require('axios');
+const { v4: uuidv4, v4 } = require('uuid');
 
 console.log(keys);
 
+
+// Search Console API
 
 const request = {
     email: keys.client_email,
@@ -34,6 +39,10 @@ const searchConsoleQuery = async () => {
       console.log(resSearchAnalytics.data.rows);
 }
 
+
+// G3 Analytics Query
+// https://flaviocopes.com/google-analytics-api-nodejs/
+
 const analyticsQuery = async () => {
   const scopes = 'https://www.googleapis.com/auth/analytics.readonly'
   const jwt = new google.auth.JWT(process.env.CLIENT_EMAIL, null, process.env.PRIVATE_KEY, scopes);
@@ -56,4 +65,44 @@ const analyticsQuery = async () => {
 }
 
 
-analyticsQuery();
+// G3 Measurement Protocol for Sending Events
+// https://developers.google.com/analytics/devguides/collection/protocol/v1/
+
+
+const sendPageViewG3 = async (url, title, propertyId = 'UA-11167465-10') => {
+    const request = {
+      url: 'https://www.google-analytics.com/collect',
+      method: 'post',
+      params: {
+        v: 1,
+        tid: propertyId,
+        cid: '0b47ceb1-8abb-4c90-8995-57c3cdf32c14',
+        t: 'pageview',
+        dl: url,
+        dt: title,
+        ua: 'Axios User Agent'
+      }
+    }
+
+    axios(request)
+    .then(response => {
+      console.log('response', response.data, response.data.parserMessage);
+    })
+    .catch(error => {
+      console.error('error', error);
+    })
+}
+
+// G4 Analytics Query
+// Experiment with domains that I own
+
+// G4 Measurement Protocol for Sending Events
+// https://developers.google.com/analytics/devguides/collection/protocol/ga4
+
+
+
+// playground
+
+sendPageViewG3('https://dev.pymnts.com/today-on-pymnts/', 'Today on Pymnts', 'UA-11167465-10');
+//analyticsQuery();
+
