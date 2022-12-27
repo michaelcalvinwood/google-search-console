bf3a0356dbc86253165e7670282491d969e533b7
Michael Wood <michaelwood33311@icloud.com>
2022-12-27 12:11:19 -0500

    added google search ranking functions via scaleserp api

---
+++ scaleSerp.js
@@ -0,0 +1,40 @@
+require ('dotenv').config();
+const axios = require('axios');
+
+
+function fetchResults (query, google_domain = "google.com", device = "desktop") {
+    // set up the request parameters
+    const params = {
+      api_key: process.env.SCALE_SERP_API_KEY,
+      q: query,
+      location: "New York,New York,United States",
+      google_domain,
+      gl: "us",
+      hl: "en",
+      device,
+      output: "json",
+      num: "100"
+    }
+
+    console.log(params);
+    
+    // make the http GET request to Scale SERP
+    return axios.get('https://api.scaleserp.com/search', { params })
+}
+
+async function getQuery(query, google_domain = "google.com", device = "desktop") {
+    let response;
+
+    try {
+        response = await fetchResults(query, google_domain, device);
+        console.log(response.data);
+        return response.data;
+    } catch (e) {
+        console.error(e);
+        return false;
+    }
+}
+
+let result = getQuery('digital transformation');
+
+
